c6fc39c6df2b5ac0a3dda9923b36816bfa1b530c
stergina <45051285+Stergina@users.noreply.github.com>
2020-07-16 00:42:50 +0300

    Update my16BitALU.vhd

--- 16-bit ALU Program/16-bit ALU/my16BitALU.vhd
+++ 16-bit ALU Program/16-bit ALU/my16BitALU.vhd
@@ -1,55 +1,62 @@
 -- Implementing a 16-bit ALU responsible to perform AND, OR, ADD, SUB, NOR, NAND and XOR functions
 
 -- Loads necessary packages
-library ieee;
-use ieee.std_logic_1164.all;
-use work.functions_16BitALU.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE work.functions_16BitALU.ALL;
 
 -- Defines entity my16BitALU with inputs a, b, opcode and outputs Result, Overflow
-entity my16BitALU is
-	port (a, b: in std_logic_vector(15 downto 0);
-			opcode: in std_logic_vector(2 downto 0);
-			Result: out std_logic_vector(15 downto 0);
-			Overflow: out std_logic);
-end my16BitALU;
+ENTITY my16BitALU IS
+	PORT (
+		a, b : IN std_logic_vector(15 DOWNTO 0);
+		opcode : IN std_logic_vector(2 DOWNTO 0);
+		Result : OUT std_logic_vector(15 DOWNTO 0);
+		Overflow : OUT std_logic
+	);
+END my16BitALU;
 
 -- Defines function my16BitALU
-architecture structural of my16BitALU is
+ARCHITECTURE structural OF my16BitALU IS
 
--- Declares 1-bit ALU as a component
-component my1BitALU
-	port (a, b, Ainvert, Binvert, CarryIn: in std_logic;
-			Operation: in std_logic_vector(1 downto 0);
-			Result, CarryOut: out std_logic);
-end component;	
+	-- Declares 1-bit ALU as a component
+	COMPONENT my1BitALU
+		PORT (
+			a, b, Ainvert, Binvert, CarryIn : IN std_logic;
+			Operation : IN std_logic_vector(1 DOWNTO 0);
+			Result, CarryOut : OUT std_logic
+		);
+	END COMPONENT; 
 
--- Declares inputs to be used in the design
-signal Ainvert, Binvert, CarryIn, CarryOut: std_logic;
-signal Operation: std_logic_vector(1 downto 0);
-signal result1BitALU, cin, cout: std_logic_vector(15 downto 0);
+	-- Declares inputs to be used in the design
+	SIGNAL Ainvert, Binvert, CarryIn, CarryOut : std_logic;
+	SIGNAL Operation : std_logic_vector(1 DOWNTO 0);
+	SIGNAL result1BitALU, cin, cout : std_logic_vector(15 DOWNTO 0);
 
--- Architecture body
-begin
+	-- Architecture body
+BEGIN
 	-- Implements the control circuit that declares the function that will take place based on the operation code (opcode)
-	Output1: myControlCircuit port map (opcode,Ainvert,Binvert,CarryIn,Operation);
+	Output1 : myControlCircuit
+	PORT MAP(opcode, Ainvert, Binvert, CarryIn, Operation);
 
 	-- Isolates the first realization of my1BitALU from the loop. We perform my1BitALU function for the first bit of each input a and b
-	Output2: my1BitALU port map(a(0),b(0),Ainvert,Binvert,CarryIn,Operation,result1BitALU(0),cout(0));	
-			Result(0) <= result1BitALU(0);
-			cin(0) <= cout(0);
-			
+	Output2 : my1BitALU
+	PORT MAP(a(0), b(0), Ainvert, Binvert, CarryIn, Operation, result1BitALU(0), cout(0)); 
+	Result(0) <= result1BitALU(0);
+	cin(0) <= cout(0);
+ 
 	-- Performs my1BitALU function for the remaining 15 bits of each input (a and b)
-		for_loop: 
-			for i in 1 to 15 generate	 
-				Output3: my1BitALU port map (a(i),b(i),Ainvert,Binvert,cin(i-1),Operation,result1BitALU(i),cout(i)); 
-				
-				-- Result of the function between the i-th bit of input a and input b
-				Result(i) <= result1BitALU(i);
-				
-				-- CarryIn of the i-th repetition equals with the CarryOut of the previous repetition
-				cin(i) <= cout(i);
-			end generate; 
-							
-	-- Calculates overflow as the result of XOR function between the CarryOuts of the last two result digits 
-	Overflow <= (cout(14) xor cout(15));
-end structural;
+	for_loop : 
+	FOR i IN 1 TO 15 GENERATE 
+		Output3 : my1BitALU
+		PORT MAP(a(i), b(i), Ainvert, Binvert, cin(i - 1), Operation, result1BitALU(i), cout(i));
+ 
+		-- Result of the function between the i-th bit of input a and input b
+		Result(i) <= result1BitALU(i);
+ 
+		-- CarryIn of the i-th repetition equals with the CarryOut of the previous repetition
+		cin(i) <= cout(i);
+END GENERATE;
+ 
+-- Calculates overflow as the result of XOR function between the CarryOuts of the last two result digits
+Overflow <= (cout(14) XOR cout(15));
+END structural;
